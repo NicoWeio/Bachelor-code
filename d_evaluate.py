b7868db02084b5cedc2c5b4809b4962618849f59
Nicolai Weitkemper <nicolai.weitkemper@tu-dortmund.de>
2022-07-02 12:20:05 +0200

    eval: plot some single event samples

--- d_evaluate.py
+++ d_evaluate.py
@@ -28,7 +28,7 @@ def spectrum_from_probas(probas):
     return np.sum(probas, axis=0)
 
 
-# Energy distribution
+# █ Energy distribution
 true_spectrum = spectrum_from_labels(labels)
 pred_spectrum = spectrum_from_probas(predicted_probas)
 pred_spectrum_class = spectrum_from_labels(predicted_labels)
@@ -41,6 +41,7 @@ axs[0].plot(BINS, true_spectrum, drawstyle='steps-mid', color='red', zorder=10, 
 # axs[0].plot(BINS, spectrum_from_labels(predicted_labels), drawstyle='steps-mid', color='royalblue', label='predicted class')
 axs[0].plot(BINS, pred_spectrum, drawstyle='steps-mid', color='green', label='predicted probas')
 axs[0].set_ylabel('count')
+axs[0].set_yscale('log')
 
 axs[1].bar(BINS, (pred_spectrum - true_spectrum) / true_spectrum, label="relative deviation")
 
@@ -57,7 +58,46 @@ plt.savefig(f'build/corn__hist_log_{run_id()}.png')
 # plt.show()
 
 
-# Confusion matrix
+# █ Single events
+SINGLE_EVENTS_GRIDSIZE = (4, 4)
+SINGLE_EVENTS_NUM = SINGLE_EVENTS_GRIDSIZE[0] * SINGLE_EVENTS_GRIDSIZE[1]
+
+# → one sample for each class
+events_per_class = SINGLE_EVENTS_NUM // NUM_BINS # not rounding up so some subplots will be empty
+# events_per_class = SINGLE_EVENTS_NUM // NUM_BINS + 1 # rounding up so the plot is filled
+sample_indices = []
+for i in range(NUM_BINS):
+    possible_indices = np.where(labels == i)[0]
+    sample_indices += np.random.choice(possible_indices, events_per_class, replace=False).tolist()
+
+# → random samples
+# sample_indices = np.random.choice(len(labels), SINGLE_EVENTS_NUM, replace=False).tolist()
+
+fig, axs = plt.subplots(
+    # SINGLE_EVENTS_GRIDSIZE[0], SINGLE_EVENTS_GRIDSIZE[1],
+    *SINGLE_EVENTS_GRIDSIZE,
+    figsize=(SINGLE_EVENTS_GRIDSIZE[1] * 5, SINGLE_EVENTS_GRIDSIZE[0] * 5)
+)
+for i, ax in enumerate(axs.flat):
+    if i >= len(sample_indices):
+        ax.axis('off')
+        continue
+
+    sample_i = sample_indices[i]
+    ax.axvline(labels[sample_i], color='red', linestyle='--', label='true class')
+    ax.plot(BINS, predicted_probas[sample_i], drawstyle='steps-mid', color='green', label='predicted class')
+    ax.set_xlabel('class')
+    ax.set_ylabel('count')
+    ax.set_yscale('log')
+    ax.grid()
+    ax.legend()
+plt.tight_layout()
+plt.savefig(f'build/corn__single_events_{run_id()}.pdf')
+plt.savefig(f'build/corn__single_events_{run_id()}.png')
+
+
+# █ Confusion matrix
+# TODO: Use predicted_probas instead of predicted_labels
 confusion_mtx = confusion_matrix(labels, predicted_labels)
 df_cm = pd.DataFrame(confusion_mtx/confusion_mtx.sum(axis=1)[:, np.newaxis])
 plt.figure(figsize=(12, 10))
