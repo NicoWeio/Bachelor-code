ac6aaa88938c1db01281cb58667cc1a4f8a0d4a4
Nicolai Weitkemper <nicolai.weitkemper@tu-dortmund.de>
2022-07-31 15:02:41 +0200

    plug-in replacement for `c_dsea`, but without DSEA
    For testing purposes

--- c_non_dsea.py
+++ c_non_dsea.py
@@ -1,9 +1,31 @@
-classifier.fit(X_train, y_train)
-y_test_pred = classifier.predict_proba(X_test)
-f_test_pred = np.bincount(y_test) / wandb.config.num_bins
+import logging
+import numpy as np
+import wandb
+#
+import c_corn
 
-probas = y_test_pred
+logging.getLogger("pytorch_lightning").setLevel(logging.ERROR)
 
 
-dist = util.chi2s(f_test_true, f_test_pred)
-print(f"Chi2 distance: {dist:.10f}")
+def run(X_train, X_test, y_train):
+    classifier = c_corn.CornClassifier(
+        input_size=X_train.shape[1],
+        num_classes=np.bincount(y_train).shape[0],
+    )
+
+    classifier.fit(X_train, y_train)
+
+    probas = classifier.predict_proba(X_test)
+
+    return probas
+
+
+
+# y_test_pred = classifier.predict_proba(X_test)
+# f_test_pred = np.bincount(y_test) / wandb.config.num_bins
+
+# probas = y_test_pred
+
+
+# dist = util.chi2s(f_test_true, f_test_pred)
+# print(f"Chi2 distance: {dist:.10f}")
